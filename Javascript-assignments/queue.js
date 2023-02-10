484d0e284bdfa3c4d16daa932d0cf72e8b920909
Harika <lakshmimaha683@gmail.com>
2023-02-10 10:29:43 +0530

    Updated Assignments

--- Javascript-assignments/queue.js
+++ Javascript-assignments/queue.js
@@ -2,19 +2,19 @@ let stack1 = [];
 let stack2 = [];
 function enqueue(element) {
   stack1.push(element);
-  console.log("Stack-1 elements are in enqueue: ", stack1);
+  console.log("Enqueue Elements: ", stack1);
 }
 function dequeue() {
   if (stack2.length === 0) {
     if (stack1.length === 0) {
-      console.log("Dequeue not possible because queue is empty..");
+      console.log("Queue Underflow");
     }
     while (stack1.length > 0) {
       let x = stack1.pop();
       stack2.push(x);
     }
   }
-  console.log("Element after Dequeue: " + stack2.pop());
+  console.log("Dequeue Operation: " + stack2.pop());
 }
 
 enqueue("f");
