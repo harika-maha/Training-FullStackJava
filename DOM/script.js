41902c88421d7b622ce1e479f89bc83642212c3b
Harika <lakshmimaha683@gmail.com>
2023-02-19 23:13:47 +0530

    Added js to webpage

--- DOM/script.js
+++ DOM/script.js
@@ -1,9 +1,45 @@
-var line1 = document.getElementsByClassName("line-1")[0];
-console.log(line1.innerHTML);
-line1.innerHTML = "<b>WEALTH ANALYSED</b>";
+// var line1 = document.getElementsByClassName("line-1")[0];
+// console.log(line1.innerHTML);
+// line1.innerHTML = "<b>WEALTH ANALYSED</b>";
 
-var ptags = document.getElementsByTagName("p")[0];
-ptags.style.color = "black";
+// var ptags = document.getElementsByTagName("p")[0];
+// ptags.style.color = "black";
 
-var line2 = document.getElementById("line2");
-line2.innerHTML = "HELLO WORLD!";
+// var line2 = document.getElementById("line2");
+// line2.innerHTML = "HELLO WORLD!";
+
+var modalbg = document.querySelector(".modal-bg");
+var login = document.getElementById("login");
+var modalClose = document.querySelector(".modal-close");
+login.addEventListener("click", () => {
+  modalbg.classList.add("bg-active");
+});
+modalClose.addEventListener("click", () => {
+  modalbg.classList.remove("bg-active");
+});
+var log = {
+  userid: "admin",
+  password: "admin",
+};
+
+var logUser = {
+  userid: "user",
+  password: "user",
+};
+
+var users = [log, logUser];
+
+var btn = document.querySelector(".buttonLogin");
+
+btn.addEventListener("click", () => {
+  var id = document.querySelector(".userid");
+  var pwd = document.querySelector(".password");
+  var name = document.querySelector(".name");
+  var currUser = users.find((user) => user.userid === id.value);
+  if (pwd.value == currUser.password) {
+    modalbg.classList.remove("bg-active");
+    name.innerHTML = `Welcome, <span style="color:orange;">${currUser.userid}</span>`;
+  }
+  id.value = "";
+  pwd.value = "";
+});
