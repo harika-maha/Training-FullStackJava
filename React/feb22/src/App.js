3afd3211835034aa2675ad379907efdb2b0b8c1e
Harika <lakshmimaha683@gmail.com>
2023-02-22 10:29:43 +0530

    Updated task

--- React/feb22/src/App.js
+++ React/feb22/src/App.js
@@ -10,7 +10,10 @@ export default class App extends Component {
       d: 4,
     };
   }
-
+  initialA = 1;
+  initialB = 2;
+  initialC = 3;
+  initialD = 4;
   changeA = () => {
     this.setState({ a: 2 });
   };
@@ -24,14 +27,33 @@ export default class App extends Component {
     this.setState({ d: 5 });
   };
   render() {
-    var sum = this.state.a + this.state.b + this.state.c + this.state.d;
+    var sum = this.initialA + this.initialB + this.initialC + this.initialD;
+    var sumAChanged =
+      this.state.a + this.initialB + this.initialC + this.initialD;
+    var sumBChanged =
+      this.initialA + this.state.b + this.initialC + this.initialD;
+    var sumCChanged =
+      this.initialA + this.initialB + this.state.c + this.initialD;
+    var sumDChanged =
+      this.initialA + this.initialB + this.initialC + this.state.d;
+    var sumAllChanged =
+      this.state.a + this.state.b + this.state.c + this.state.d;
     return (
       <>
         <h1>SUM: {sum}</h1>
         <button onClick={this.changeA}>Change A</button>
+        <h1>Sum with A changed: {sumAChanged}</h1>
+        <hr></hr>
         <button onClick={this.changeB}>Change B</button>
+        <h1>Sum with B changed: {sumBChanged}</h1>
+        <hr></hr>
         <button onClick={this.changeC}>Change C</button>
+        <h1>Sum with C changed: {sumCChanged}</h1>
+        <hr></hr>
         <button onClick={this.changeD}>Change D</button>
+        <h1>Sum with D changed: {sumDChanged}</h1>
+        <hr></hr>
+        <h1>Sum with current states: {sumAllChanged}</h1>
       </>
     );
   }
